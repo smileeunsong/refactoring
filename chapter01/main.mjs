7f231e6ac13df5ef80d406ad664d9234c3f2c000
Eunsong-Park <run.eunsong@gmail.com>
2023-06-04 15:16:48 +0900

    feat: chapter01-1 추가

---
+++ chapter01/main.mjs
@@ -0,0 +1,8 @@
+import { statement } from "./01-1/statement.js";
+import invoices from "./invoices.json" assert { type: "json" };
+import plays from "./plays.json" assert { type: "json" };
+
+const result = statement(invoices[0], plays);
+
+console.log("\n\n");
+console.log(result);
