1e6f87a5166292211634c8b109e6c7613ab71d92
Eunsong-Park <run.eunsong@gmail.com>
2023-06-06 16:38:05 +0900

    feat: mocha test 코드 추가

---
+++ chapter01/test/statement.test.mjs
@@ -0,0 +1,19 @@
+import { assert } from "chai";
+import { statement } from "../01-1/statement.js";
+import invoices from "../invoices.json" assert { type: "json" };
+import plays from "../plays.json" assert { type: "json" };
+
+describe("statement 함수 테스트", () => {
+  it("청구내역을 정확히 출력하는지 확인", () => {
+    let expected = `청구내역 (고객명: ${invoices[0].customer})\n`;
+    expected += "Hamlet: $650.00 55석\n";
+    expected += "As You Like It: $580.00 35석\n";
+    expected += "Othello: $500.00 40석\n";
+    expected += "총액: $1,730.00\n";
+    expected += "적립 포인트: 47점\n";
+
+    const result = statement(invoices[0], plays);
+
+    assert.strictEqual(result, expected);
+  });
+});
