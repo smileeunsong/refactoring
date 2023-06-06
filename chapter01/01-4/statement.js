1637d822a7f4ea28b77bf980d4aa36146a1ebe08
Eunsong-Park <run.eunsong@gmail.com>
2023-06-06 20:14:25 +0900

    refactor: 01-4, totalAmount 변수 제거

--- chapter01/01-4/statement.js
+++ chapter01/01-4/statement.js
@@ -1,5 +1,4 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
   function amountFor(aPerformance) {
@@ -59,15 +58,22 @@ export function statement(invoice, plays) {
     return volumeCredits;
   }
 
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
+
   for (let perf of invoice.performances) {
     // 청구 내역을 출력한다.
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} ${
       perf.audience
     }석\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
 
   return result;
