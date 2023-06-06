e9e2b295b68bc8119aa661511bc9573b5a0b4e69
Eunsong-Park <run.eunsong@gmail.com>
2023-06-07 00:28:25 +0900

    doc: 01-10, 정리 노트 추가

--- chapter01/01-8/createStatementData.js
+++ chapter01/01-8/createStatementData.js
@@ -31,6 +31,17 @@ export default function createStatementData(invoice, plays) {
   }
 }
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
+}
+
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
@@ -43,17 +54,6 @@ class PerformanceCalculator {
 
   get volumeCredits() {
     return Math.max(this.performance.audience - 30, 0);
-  }
-}
-
-function createPerformanceCalculator(aPerformance, aPlay) {
-  switch (aPlay.type) {
-    case "tragedy":
-      return new TragedyCalculator(aPerformance, aPlay);
-    case "comedy":
-      return new ComedyCalculator(aPerformance, aPlay);
-    default:
-      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
   }
 }
 
