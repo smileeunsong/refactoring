f0e02903e8516112206bd04454b2e4cf7285b13a
Eunsong-Park <run.eunsong@gmail.com>
2023-06-06 18:07:49 +0900

    refactor: 01-4, 명확한 변수 이름 변경

--- chapter01/01-4/statement.js
+++ chapter01/01-4/statement.js
@@ -33,27 +33,27 @@ export function statement(invoice, plays) {
 
 function amountFor(perf, play) {
   // <- 값이 바뀌지 않는 변수는 매개변수로 전달
-  let thisAmount = 0; // <- 변수를 초기화하는 코드
+  let result = 0; // <- 변수를 초기화하는 코드, 명확한 이름으로 변경
 
   switch (play.type) {
     case "tragedy":
-      thisAmount = 40000;
+      result = 40000;
 
       if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+        result += 1000 * (perf.audience - 30);
       }
       break;
     case "comedy":
-      thisAmount = 30000;
+      result = 30000;
 
       if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+        result += 10000 + 500 * (perf.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * perf.audience;
       break;
 
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
   }
-  return thisAmount; // <- 함수 안에서 값이 바뀌는 변수 반환
+  return result; // <- 함수 안에서 값이 바뀌는 변수 반환
 }
