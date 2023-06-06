5f7ab0dbb26ea0cdbe1942c6373b5fa9691f2bea
Eunsong-Park <run.eunsong@gmail.com>
2023-06-07 00:00:34 +0900

    refactor: 01-8, TragedyCalculator 서브클래스 메서드 오버라이드

--- chapter01/01-8/createStatementData.js
+++ chapter01/01-8/createStatementData.js
@@ -49,18 +49,11 @@ class PerformanceCalculator {
 
   get amount() {
     let result = 0;
-
     switch (this.play.type) {
       case "tragedy":
-        result = 40000;
-
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
+        throw "오류 발생";
       case "comedy":
         result = 30000;
-
         if (this.performance.audience > 20) {
           result += 10000 + 500 * (this.performance.audience - 20);
         }
@@ -95,6 +88,14 @@ function createPerformanceCalculator(aPerformance, aPlay) {
   }
 }
 
-class TragedyCalculator extends PerformanceCalculator {}
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
 
 class ComedyCalculator extends PerformanceCalculator {}
