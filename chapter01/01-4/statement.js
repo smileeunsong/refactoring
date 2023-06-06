2d180307479277b7f317675b1cd4c87c033205a9
Eunsong-Park <run.eunsong@gmail.com>
2023-06-06 19:22:05 +0900

    refactor: 01-4, 반복문 쪼개기 - 문장 슬라이드 하기 - 함수로 추출

--- chapter01/01-4/statement.js
+++ chapter01/01-4/statement.js
@@ -1,6 +1,6 @@
 export function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
+
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
   function amountFor(aPerformance) {
@@ -44,24 +44,33 @@ export function statement(invoice, plays) {
     return result;
   }
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
+  }
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
   }
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf); // <- 추출한 함수를 이용해 값을 누적
-
     // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} ${
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} ${
       perf.audience
     }석\n`;
     totalAmount += amountFor(perf);
   }
-  result += `총액: ${format(totalAmount / 100)}\n`;
+
+  let volumeCredits = totalVolumeCredits();
+
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
 
   return result;
