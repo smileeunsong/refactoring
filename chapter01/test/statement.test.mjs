b72fb998c47b865351e4994459b4395ed882bb06
Eunsong-Park <run.eunsong@gmail.com>
2023-06-06 17:57:07 +0900

    refactor: 01-4, 함수 추출하기

--- chapter01/test/statement.test.mjs
+++ chapter01/test/statement.test.mjs
@@ -1,5 +1,5 @@
 import { assert } from "chai";
-import { statement } from "../01-1/statement.js";
+import { statement } from "../01-4/statement.js";
 import invoices from "../invoices.json" assert { type: "json" };
 import plays from "../plays.json" assert { type: "json" };
 
