5e724c6796823ae5861b8f545a93cecf2e9a789f
Eunsong-Park <run.eunsong@gmail.com>
2023-06-06 23:42:21 +0900

    refactor: 01-8, volumeCredits() 이사

--- chapter01/01-8/createStatementData.js
+++ chapter01/01-8/createStatementData.js
@@ -12,9 +12,9 @@ export default function createStatementData(invoice, plays) {
       playFor(aPerformance)
     );
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
@@ -28,13 +28,8 @@ export default function createStatementData(invoice, plays) {
   }
 
   function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-
-    if ("comedy" === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance))
+      .volumeCredits;
   }
 
   function totalAmount(data) {
@@ -74,6 +69,16 @@ class PerformanceCalculator {
 
       default:
         throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+
+    if ("comedy" === this.play.type) {
+      result += Math.floor(this.performance.audience / 5);
     }
     return result;
   }
