e6c40d5a4d008cfc5aace13ca3ef1fd158639e3f
Eunsong-Park <run.eunsong@gmail.com>
2023-06-17 23:33:39 +0900

    feat: 04-4, profit 테스트 추가

---
+++ chapter04/04-4/test.js
@@ -0,0 +1,17 @@
+import { Province, sampleProvinceData } from "./index.js";
+import assert from "assert";
+
+describe("province", function () {
+  let asia;
+  beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
+
+  it("shortfall", function () {
+    assert.equal(asia.shortfall, 5);
+  });
+
+  it("profit", function () {
+    assert.equal(asia.profit, 230);
+  });
+});
