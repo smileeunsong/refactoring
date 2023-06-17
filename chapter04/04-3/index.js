a32c56f6205cebb8bcb8b03a4a0293c01a93aca3
Eunsong-Park <run.eunsong@gmail.com>
2023-06-17 13:49:11 +0900

    feat: 04-3 test 코드 연결

--- chapter04/04-3/index.js
+++ chapter04/04-3/index.js
@@ -1,4 +1,4 @@
-class Province {
+export class Province {
   constructor(doc) {
     this._name = doc.name;
     this._producers = [];
@@ -91,7 +91,7 @@ class Producer {
   }
 }
 
-function sampleProvinceData() {
+export function sampleProvinceData() {
   return {
     name: "Asia",
     producers: [
