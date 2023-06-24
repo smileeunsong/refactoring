63431760a383e677eacc6103db4ae2a7b1467b94
Eunsong-Park <run.eunsong@gmail.com>
2023-06-24 23:45:34 +0900

    feat: 06-1 함수 추출하기, 배너 출력 로직을 함수로 추출

--- chapter06/06-01/refactoring.js
+++ chapter06/06-01/refactoring.js
@@ -4,9 +4,7 @@ export function printOwing(invoice) {
   let outstanding = 0;
   let result = "";
 
-  result += `***********************\n`;
-  result += `****** 고객 채무 ******\n`;
-  result += `***********************\n`;
+  printBanner();
 
   // 미해결 채무(outstanding)를 계산한다.
   for (const o of invoice.orders) {
@@ -25,6 +23,12 @@ export function printOwing(invoice) {
   result += `고객명: ${invoice.customer}\n`;
   result += `채무액: ${outstanding}\n`;
   result += `마감일: ${invoice.dueDate.toLocaleDateString()}\n`;
+
+  function printBanner() {
+    result += `***********************\n`;
+    result += `****** 고객 채무 ******\n`;
+    result += `***********************\n`;
+  }
 
   return result;
 }
