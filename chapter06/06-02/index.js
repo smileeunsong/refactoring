9524f205c30ae0cfa7883aba4034b8694bd47e43
Eunsong-Park <run.eunsong@gmail.com>
2023-06-25 01:03:25 +0900

    feat: 06-2 함수 인라인하기, 리팩터링

--- chapter06/06-02/index.js
+++ chapter06/06-02/index.js
@@ -2,6 +2,17 @@ function rating(aDriver) {
   return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
 }
 
-function moreThanFiveLateDeliveries(aDriver) {
-  return aDriver.numberOfLateDeliveries > 5;
+function moreThanFiveLateDeliveries(dvr) {
+  return dvr.numberOfLateDeliveries > 5;
+}
+
+function reportLines(aCustomer) {
+  const lines = [];
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+}
+
+function gatherCustomerData(out, aCustomer) {
+  out.push(["name", aCustomer.name]);
+  out.push(["location", aCustomer.location]);
 }
--- chapter06/06-02/refactoring.js
+++ chapter06/06-02/refactoring.js
@@ -1,7 +1,10 @@
 function rating(aDriver) {
-  return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
+  return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
 
-function moreThanFiveLateDeliveries(aDriver) {
-  return aDriver.numberOfLateDeliveries > 5;
+function reportLines(aCustomer) {
+  const lines = [];
+  lines.push(["name", aCustomer.name]);
+  lines.push(["location", aCustomer.location]);
+  return lines;
 }
