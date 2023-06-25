dad62464ee3230506a5ce1f42d574e70af28c68d
Eunsong-Park <run.eunsong@gmail.com>
2023-06-25 22:12:11 +0900

    feat: 06-6 변수 캡슐화 하기, 데이터 접근 함수 정의

--- chapter06/06-06/refactoring.js
+++ chapter06/06-06/refactoring.js
@@ -3,3 +3,11 @@ let defaultOwner = { firstName: "마틴", lastName: "파울러" };
 spaceship.owner = defaultOwner;
 
 defaultOwner = { firstName: "레베카", lastName: "파슨스" };
+
+function getDefaultOwner() {
+  return defaultOwner;
+}
+
+function setDefaultOwner(arg) {
+  defaultOwner = arg;
+}
