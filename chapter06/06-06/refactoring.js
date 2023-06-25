e1ca25e9e7d23ddb3518249e91e7926be87a0562
Eunsong-Park <run.eunsong@gmail.com>
2023-06-25 22:13:50 +0900

    feat: 06-6 변수 캡슐화 하기, 게터 세터함수 호출하는 것으로 고침

--- chapter06/06-06/refactoring.js
+++ chapter06/06-06/refactoring.js
@@ -1,8 +1,8 @@
 let defaultOwner = { firstName: "마틴", lastName: "파울러" };
 
-spaceship.owner = defaultOwner;
+spaceship.owner = getDefaultOwner();
 
-defaultOwner = { firstName: "레베카", lastName: "파슨스" };
+setDefaultOwner({ firstName: "레베카", lastName: "파슨스" });
 
 function getDefaultOwner() {
   return defaultOwner;
