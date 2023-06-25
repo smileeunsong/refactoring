0d0c23c98ca59029ac09b5219b11fdf7574ac232
Eunsong-Park <run.eunsong@gmail.com>
2023-06-25 18:54:01 +0900

    feat: 06-5 함수 선언 바꾸기, 리팩터링

--- chapter06/06-05/index.js
+++ chapter06/06-05/index.js
@@ -1,3 +1,22 @@
+// 함수 이름 바꾸기
 function circum(radius) {
   return 2 * Math.PI * radius;
 }
+
+// 매개변수 추가하기
+class Book {
+  constructor() {
+    this._reservations = [];
+  }
+
+  addReservation(customer) {
+    this._reservations.push(customer);
+  }
+}
+
+// 매개변수를 속성으로 바꾸기
+function inNewEngland(aCustomer) {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(aCustomer.address.state);
+}
+
+const newEnglanders = someCustomers.filter((c) => inNewEngland(c));
--- chapter06/06-05/refactoring.js
+++ chapter06/06-05/refactoring.js
@@ -1,3 +1,25 @@
-function circum(radius) {
+// 함수 이름 바꾸기
+function circumference(radius) {
   return 2 * Math.PI * radius;
 }
+
+// 매개변수 추가하기
+class Book {
+  constructor() {
+    this._reservations = [];
+  }
+
+  addReservation(customer, isPriority) {
+    assert(isPriority === true || isPriority === false);
+    this._reservations.push(customer);
+  }
+}
+
+// 매개변수를 속성으로 바꾸기
+function inNewEngland(stateCode) {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
+}
+
+const newEnglanders = someCustomers.filter((c) =>
+  inNewEngland(c.address.state)
+);
