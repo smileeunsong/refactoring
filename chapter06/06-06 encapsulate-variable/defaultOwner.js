77514f928b2ad97f5089d1d345bea37e3c755e01
Eunsong-Park <run.eunsong@gmail.com>
2023-06-25 22:46:38 +0900

    feat: 06-06 변수 캡슐화하기, 게터를 데이터 복제본 반환하는 것으로 수정

--- chapter06/06-06 encapsulate-variable/defaultOwner.js
+++ chapter06/06-06 encapsulate-variable/defaultOwner.js
@@ -1,6 +1,6 @@
 let defaultOwner = { firstName: "마틴", lastName: "파울러" };
 export function DefaultOwner() {
-  return defaultOwner;
+  return Object.assign({}, defaultOwner);
 }
 export function setDefaultOwner(arg) {
   defaultOwner = arg;
