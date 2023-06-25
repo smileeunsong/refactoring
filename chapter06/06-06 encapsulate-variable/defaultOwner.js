33a866984df536457c97fe6f968ad346efbf9576
Eunsong-Park <run.eunsong@gmail.com>
2023-06-25 23:14:06 +0900

    fix: 06-06 변수 캡슐화하기, 코드 부분 수정

--- chapter06/06-06 encapsulate-variable/defaultOwner.js
+++ chapter06/06-06 encapsulate-variable/defaultOwner.js
@@ -1,7 +1,20 @@
-let defaultOwner = { firstName: "마틴", lastName: "파울러" };
-export function DefaultOwner() {
-  return Object.assign({}, defaultOwner);
+let defaultOwnerData = { firstName: "마틴", lastName: "파울러" };
+export function defaultOwner() {
+  return new Person(defaultOwnerData);
 }
 export function setDefaultOwner(arg) {
-  defaultOwner = arg;
+  defaultOwnerData = arg;
+}
+
+class Person {
+  constructor(data) {
+    this._lastName = data.lastName;
+    this._firstName = data.firstName;
+  }
+  get lastName() {
+    return this._lastName;
+  }
+  get firstName() {
+    return this._firstName;
+  }
 }
--- chapter06/06-06 encapsulate-variable/refactoring.js
+++ chapter06/06-06 encapsulate-variable/refactoring.js
@@ -1,3 +1,3 @@
-spaceship.owner = getDefaultOwner();
+spaceship.owner = defaultOwner();
 
 setDefaultOwner({ firstName: "레베카", lastName: "파슨스" });
