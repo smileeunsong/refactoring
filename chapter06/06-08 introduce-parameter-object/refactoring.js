5e755176b9e09eb86e648676334c5b77a3c75f56
Eunsong-Park <run.eunsong@gmail.com>
2023-07-01 16:02:18 +0900

    feat: 06-08, 묶은 데이터 클래스 선언

--- chapter06/06-08 introduce-parameter-object/refactoring.js
+++ chapter06/06-08 introduce-parameter-object/refactoring.js
@@ -18,3 +18,17 @@ alerts = readingsOutsideRange(
   operatingPlan.temperatureFloor,
   operatingPlan.temperatureCeiling
 );
+
+class NumberRange {
+  constructor(min, max) {
+    this._data = { min: min, max: max };
+  }
+
+  get min() {
+    return this._data.min;
+  }
+
+  get max() {
+    return this._data.max;
+  }
+}
