d5af6e6809cd2b81dda24411327235a7caa28f0f
Eunsong-Park <run.eunsong@gmail.com>
2023-07-16 00:41:36 +0900

    feat: 07-06, 예시 추가

---
+++ chapter07/07-06 inline-class/index.js
@@ -0,0 +1,42 @@
+class TrackingInformation {
+  get shippingCompany() {
+    return this._shippingCompany;
+  }
+
+  set shippingCompany(arg) {
+    this._shippingCompany = arg;
+  }
+
+  get trackingNumber() {
+    return this._trackingNumber;
+  }
+
+  set trackingNumber(arg) {
+    this._trackingNumber = arg;
+  }
+
+  get display() {
+    return `${this.shippingCompany}: ${this.trackingNumber}`;
+  }
+}
+
+class Shipment {
+  constructor() {
+    this._trackingInformation = new TrackingInformation();
+  }
+
+  get trackingInfo() {
+    return this._trackingInformation.display;
+  }
+
+  get trackingInformation() {
+    return this._trackingInformation;
+  }
+
+  set trackingInformation(aTrackingInformation) {
+    this._trackingInformation = aTrackingInformation;
+  }
+}
+
+const aShipment = new Shipment();
+aShipment.trackingInformation.shippingCompany = request.vendor;
---
+++ chapter07/07-06 inline-class/refactoring.js
@@ -0,0 +1,24 @@
+class Shipment {
+  get trackingInfo() {
+    return `${this.shippingCompany}: ${this.trackingNumber}`;
+  }
+
+  get shippingCompany() {
+    return this._shippingCompany;
+  }
+
+  set shippingCompany(arg) {
+    this._shippingCompany = arg;
+  }
+
+  get trackingNumber() {
+    return this._trackingNumber;
+  }
+
+  set trackingNumber(arg) {
+    this._trackingNumber = arg;
+  }
+}
+
+const aShipment = new Shipment();
+aShipment.shippingCompany = request.vendor;
