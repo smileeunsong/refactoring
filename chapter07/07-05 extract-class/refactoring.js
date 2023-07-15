9057857d4d654de00fcade5c6c5c9c85525f240e
Eunsong-Park <run.eunsong@gmail.com>
2023-07-15 23:43:33 +0900

    feat: 07-05, 예시 추가

---
+++ chapter07/07-05 extract-class/refactoring.js
@@ -0,0 +1,55 @@
+class Person {
+  constructor() {
+    this._telephoneNumber = new TelephoneNumber();
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(arg) {
+    this._name = arg;
+  }
+
+  get telephoneNumber() {
+    return this._telephoneNumber.toString();
+  }
+
+  get officeAreaCode() {
+    return this._telephoneNumber.areaCode;
+  }
+
+  set officeAreaCode(arg) {
+    this._telephoneNumber.areaCode = arg;
+  }
+
+  get officeNumber() {
+    return this._telephoneNumber.number;
+  }
+
+  set officeNumber(arg) {
+    this._telephoneNumber.number = arg;
+  }
+}
+
+class TelephoneNumber {
+  get areaCode() {
+    return this._areaCode;
+  }
+
+  set areaCode(arg) {
+    this._areaCode = arg;
+  }
+
+  get number() {
+    return this._number;
+  }
+
+  set number(arg) {
+    this._number = arg;
+  }
+
+  toString() {
+    return `(${this.areaCode}) ${this.number}`;
+  }
+}
