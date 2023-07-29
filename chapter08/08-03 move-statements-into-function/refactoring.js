c8633e3ce635ab70f2929f06c3f4f862c5dfc857
Eunsong-Park <run.eunsong@gmail.com>
2023-07-29 17:04:56 +0900

    feat: 08-03, 문장을 함수로 옮기기 예시 추가

---
+++ chapter08/08-03 move-statements-into-function/refactoring.js
@@ -0,0 +1,20 @@
+// 사진 관련 데이터를 HTML로 내보내는 코드
+function renderPerson(outStream, person) {
+  const result = [];
+  result.push(`<p>${person.name}</p>`);
+  result.push(renderPhoto(person.photo));
+  result.push(emitPhotoData(person.photo));
+  return result.join("\n");
+}
+
+function photoDiv(aPhoto) {
+  return ["<div>", emitPhotoData(aPhoto), "</div>"].join("\n");
+}
+
+function emitPhotoData(aPhoto) {
+  return [
+    `<p>제목: ${aPhoto.title}</p>`,
+    `<p>위치: ${aPhoto.location}</p>`,
+    `<p>날짜: ${aPhoto.date.toDateString()}</p>`,
+  ].join("\n");
+}
