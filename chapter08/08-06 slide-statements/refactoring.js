bba349cc9a18ba7d63786999887e1e304ccc5a35
Eunsong-Park <run.eunsong@gmail.com>
2023-07-29 18:19:16 +0900

    feat: 08-06, 문장 슬라이드하기 예시 추가

---
+++ chapter08/08-06 slide-statements/refactoring.js
@@ -0,0 +1,8 @@
+let result;
+if (availableResources.length === 0) {
+  result = createResource();
+} else {
+  result = availableResources.pop();
+}
+allocatedResources.push(result);
+return result;
