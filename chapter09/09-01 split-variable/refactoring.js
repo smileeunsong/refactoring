105d8b26a8d8d8faed86fcde67625add0284ac26
Eunsong-Park <run.eunsong@gmail.com>
2023-08-06 00:01:01 +0900

    feat: 09-01, 첫번째 리팩터링 예시 추가

--- chapter09/09-01 split-variable/refactoring.js
+++ chapter09/09-01 split-variable/refactoring.js
@@ -1,18 +1,20 @@
 function distanceTravelled(scenario, time) {
   let result;
-  let acc = scenario.primaryForce / scenario.mass;
+  const primaryAcceleration = scenario.primaryForce / scenario.mass;
   let primaryTime = Math.min(time, scenario.delay);
 
-  result = 0.5 * acc * primaryTime * primaryTime;
+  result = 0.5 * primaryAcceleration * primaryTime * primaryTime;
 
   let secondaryTime = time - scenario.delay;
   if (secondaryTime > 0) {
-    let primaryVelocity = acc * scenario.delay;
-    acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+    let primaryVelocity = primaryAcceleration * scenario.delay;
+    const secondaryAcceleration =
+      (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
 
     result +=
       primaryVelocity * secondaryTime +
-      0.5 * acc * secondaryTime * secondaryTime;
+      0.5 * secondaryAcceleration * secondaryTime * secondaryTime;
   }
+
   return result;
 }
