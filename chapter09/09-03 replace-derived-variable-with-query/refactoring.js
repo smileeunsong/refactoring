3450411bf3247a6c6d015644aa364fadc60f026c
Eunsong-Park <run.eunsong@gmail.com>
2023-08-06 13:51:08 +0900

    feat: 09-03, 리팩터링 예시 추가

--- chapter09/09-03 replace-derived-variable-with-query/refactoring.js
+++ chapter09/09-03 replace-derived-variable-with-query/refactoring.js
@@ -7,10 +7,10 @@ class ProductionPlan {
   }
 
   get production() {
-    return this._production;
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
   }
+
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment);
-    this._production += anAdjustment.amount;
   }
 }
