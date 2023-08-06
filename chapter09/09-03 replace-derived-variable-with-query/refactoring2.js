d780807427adb3621bddb4b3562aa0822d0878b2
Eunsong-Park <run.eunsong@gmail.com>
2023-08-06 14:14:46 +0900

    feat: 09-03, 리팩터링2 예시 추가

--- chapter09/09-03 replace-derived-variable-with-query/refactoring2.js
+++ chapter09/09-03 replace-derived-variable-with-query/refactoring2.js
@@ -1,14 +1,18 @@
 class ProductionPlan {
   constructor(production) {
-    this._production = production;
+    this._initialProduction = production;
     this._adjustments = [];
   }
 
   get production() {
-    return this._production;
+    return this._initialProduction + this.calculatedProductionAccumulator;
   }
+
+  get calculatedProductionAccumulator() {
+    return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
+  }
+
   applyAdjustment(anAdjustment) {
     this._adjustments.push(anAdjustment);
-    this._production += anAdjustment.amount;
   }
 }
