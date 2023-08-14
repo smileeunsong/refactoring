5f9fd3625b8ec038beb0a7f5a73f07fc339bca79
Eunsong-Park <run.eunsong@gmail.com>
2023-08-14 15:39:09 +0900

    feat: 10-01, 조건식 쪼개기 예시 추가

---
+++ chapter10/10-01 decompose-conditional/refactoring2.js
@@ -0,0 +1,13 @@
+charge = summer() ? summerCharge() : regularCharge();
+
+function summer() {
+  return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd);
+}
+
+function summerCharge() {
+  return quantity * plan.summerRate;
+}
+
+function regularCharge() {
+  return quantity * plan.regularRate + plan.regularServiceCharge;
+}
