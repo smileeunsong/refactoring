d728aa75d201ac0f5ebd9f83594841df38c0cefd
Eunsong-Park <run.eunsong@gmail.com>
2023-08-15 19:35:01 +0900

    feat: 10-07, 제어플래그 제거

--- chapter10/10-07 replace-control-flag-with-break/refactoring.js
+++ chapter10/10-07 replace-control-flag-with-break/refactoring.js
@@ -1,15 +1,12 @@
 function checkForMiscreants(people) {
-  let found = false;
   for (const p of people) {
-    if (!found) {
-      if (p === "조커") {
-        sendAlert();
-        return;
-      }
-      if (p === "사루만") {
-        sendAlert();
-        return;
-      }
+    if (p === "조커") {
+      sendAlert();
+      return;
+    }
+    if (p === "사루만") {
+      sendAlert();
+      return;
     }
   }
 }
