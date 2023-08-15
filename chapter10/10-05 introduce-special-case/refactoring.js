4d634d3e4dfaed4a2f76c52a72bda0904a1fbbaa
Eunsong-Park <run.eunsong@gmail.com>
2023-08-15 18:40:30 +0900

    feat: 10-05, 클라이언트 코드 isUnknown() 함수 적용

--- chapter10/10-05 introduce-special-case/refactoring.js
+++ chapter10/10-05 introduce-special-case/refactoring.js
@@ -31,18 +31,18 @@ function isUnknown(arg) {
 const aCustomer = site.customer;
 // ... lots of intervening code ...
 let customerName;
-if (aCustomer === "unknown") customerName = "occupant";
+if (isUnknown(aCustomer)) customerName = "occupant";
 else customerName = aCustomer.name;
 
 // client 2...
-const plan =
-  aCustomer === "unknown" ? registry.billingPlans.basic : aCustomer.billingPlan;
+const plan = isUnknown(aCustomer)
+  ? registry.billingPlans.basic
+  : aCustomer.billingPlan;
 
 // client 3...
-if (aCustomer !== "unknown") aCustomer.billingPlan = newPlan;
+if (!isUnknown(aCustomer)) aCustomer.billingPlan = newPlan;
 
 // client 4...
-const weeksDelinquent =
-  aCustomer === "unknown"
-    ? 0
-    : aCustomer.paymentHistory.weeksDelinquentInLastYear;
+const weeksDelinquent = isUnknown(aCustomer)
+  ? 0
+  : aCustomer.paymentHistory.weeksDelinquentInLastYear;
