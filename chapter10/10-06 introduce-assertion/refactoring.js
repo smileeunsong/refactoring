5cea1c7943bfa9d9d7d509667d5d1090819ff10e
Eunsong-Park <run.eunsong@gmail.com>
2023-08-15 19:15:37 +0900

    feat: 10-06, discountRate 세터에 assertion 추가

--- chapter10/10-06 introduce-assertion/refactoring.js
+++ chapter10/10-06 introduce-assertion/refactoring.js
@@ -6,4 +6,9 @@ class Customer {
       return aNumber - this.discountRate * aNumber;
     }
   }
+
+  set discountRate(aNumber) {
+    assert(null === aNumber || aNumber >= 0);
+    this._discountRate = aNumber;
+  }
 }
