d936c7187d52c5ab6f9d960252f917b74cec4f73
Eunsong-Park <run.eunsong@gmail.com>
2023-08-15 19:34:08 +0900

    feat: 10-07, 제어 플래그 찾아서 return으로 바꾸기

--- chapter10/10-07 replace-control-flag-with-break/refactoring.js
+++ chapter10/10-07 replace-control-flag-with-break/refactoring.js
@@ -1,13 +1,15 @@
-let found = false;
-for (const p of people) {
-  if (!found) {
-    if (p === "조커") {
-      sendAlert();
-      found = true;
-    }
-    if (p === "사루만") {
-      sendAlert();
-      found = true;
+function checkForMiscreants(people) {
+  let found = false;
+  for (const p of people) {
+    if (!found) {
+      if (p === "조커") {
+        sendAlert();
+        return;
+      }
+      if (p === "사루만") {
+        sendAlert();
+        return;
+      }
     }
   }
 }
