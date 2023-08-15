c51d75e6c628e4aaa80dc456fcad12ea10a2c9fb
Eunsong-Park <run.eunsong@gmail.com>
2023-08-15 18:31:27 +0900

    feat: 10-05, 리팩터링 준비

---
+++ chapter10/10-05 introduce-special-case/refactoring.js
@@ -0,0 +1,32 @@
+class Site {
+  get customer() {
+    return this._customer;
+  }
+}
+
+class Customer {
+  get name() {}
+  get billingPlan() {}
+  set billingPlan(arg) {}
+  get paymentHistory() {}
+}
+
+// client 1...
+const aCustomer = site.customer;
+// ... lots of intervening code ...
+let customerName;
+if (aCustomer === "unknown") customerName = "occupant";
+else customerName = aCustomer.name;
+
+// client 2...
+const plan =
+  aCustomer === "unknown" ? registry.billingPlans.basic : aCustomer.billingPlan;
+
+// client 3...
+if (aCustomer !== "unknown") aCustomer.billingPlan = newPlan;
+
+// client 4...
+const weeksDelinquent =
+  aCustomer === "unknown"
+    ? 0
+    : aCustomer.paymentHistory.weeksDelinquentInLastYear;
