6166650ac397dfe428a66b31f32037f2bd9a2bb9
Eunsong-Park <run.eunsong@gmail.com>
2023-08-15 11:42:01 +0900

    feat: 10-04, 여러 함수 클래스로 묶기

--- chapter10/10-04 replace-conditional-with-polymorphism/refactoring2.js
+++ chapter10/10-04 replace-conditional-with-polymorphism/refactoring2.js
@@ -1,45 +1,56 @@
 function rating(vayage, history) {
-  const vpf = voyageProfitFactor(vayage, history);
-  const vr = voyageRisk(vayage);
-  const chr = captainHistoryRisk(vayage, history);
-  if (vpf * 3 > vr + chr * 2) return "A";
-  else return "B";
+  return new Rating(vayage, history).value;
 }
 
-function voyageRisk(vayage) {
-  let result = 1;
-  if (vayage.length > 4) result += 2;
-  if (vayage.length > 8) result += vayage.length - 8;
-  if (["china", "east-indies"].includes(vayage.zone)) result += 4;
-  return Math.max(result, 0);
-}
-
-function captainHistoryRisk(vayage, history) {
-  let result = 1;
-  if (history.length < 5) result += 4;
-  result += history.filter((v) => v.profit < 0).length;
-  if (vayage.zone === "china" && hasChina(history)) result -= 2;
-  return Math.max(result, 0);
-}
-
-function hasChina(history) {
-  return history.some((v) => "china" === v.zone);
-}
-
-function voyageProfitFactor(vayage, history) {
-  let result = 2;
-  if (vayage.zone === "china") result += 1;
-  if (vayage.zone === "east-indies") result += 1;
-  if (vayage.zone === "china" && hasChina(history)) {
-    result += 3;
-    if (history.length > 10) result += 1;
-    if (vayage.length > 12) result += 1;
-    if (vayage.length > 18) result -= 1;
-  } else {
-    if (history.length > 8) result += 1;
-    if (vayage.length > 14) result -= 1;
+class Rating {
+  constructor(vayage, history) {
+    this._vayage = vayage;
+    this._history = history;
   }
-  return result;
+
+  get value() {
+    const vpf = this.voyageProfitFactor;
+    const vr = this.voyageRisk;
+    const chr = this.captainHistoryRisk;
+    if (vpf * 3 > vr + chr * 2) return "A";
+    else return "B";
+  }
+
+  get voyageRisk() {
+    let result = 1;
+    if (this._vayage.length > 4) result += 2;
+    if (this._vayage.length > 8) result += this._vayage.length - 8;
+    if (["china", "east-indies"].includes(this._vayage.zone)) result += 4;
+    return Math.max(result, 0);
+  }
+
+  get captainHistoryRisk() {
+    let result = 1;
+    if (this._history.length < 5) result += 4;
+    result += this._history.filter((v) => v.profit < 0).length;
+    if (this._vayage.zone === "china" && this.hasChina) result -= 2;
+    return Math.max(result, 0);
+  }
+
+  get voyageProfitFactor() {
+    let result = 2;
+    if (this._vayage.zone === "china") result += 1;
+    if (this._vayage.zone === "east-indies") result += 1;
+    if (this._vayage.zone === "china" && this.hasChina) {
+      result += 3;
+      if (this._history.length > 10) result += 1;
+      if (this._vayage.length > 12) result += 1;
+      if (this._vayage.length > 18) result -= 1;
+    } else {
+      if (this._history.length > 8) result += 1;
+      if (this._vayage.length > 14) result -= 1;
+    }
+    return result;
+  }
+
+  get hasChinaHistory() {
+    return this._history.some((v) => "china" === v.zone);
+  }
 }
 
 const voyage = { zone: "west-indies", length: 10 };
