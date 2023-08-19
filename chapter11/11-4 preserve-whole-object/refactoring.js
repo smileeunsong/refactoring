2edc6f1e3ad4af3a5467e00c20527ade1148fdfc
Eunsong-Park <run.eunsong@gmail.com>
2023-08-19 16:24:26 +0900

    feat: 11-4, 리팩터링 예시 추가

--- chapter11/11-4 preserve-whole-object/refactoring.js
+++ chapter11/11-4 preserve-whole-object/refactoring.js
@@ -1,16 +1,15 @@
 // 실내온도 모니터링 시스템
 // 일일 최저 최고 기온이 난방 계획에서 정한 범위를 벗어나는지 확인
 
-const low = aRoom.daysTempRange.low;
-const high = aRoom.daysTempRange.high;
-if (!aPlan.withinRange(low, high)) {
+if (!aPlan.withinRange(aRoom.daysTempRange)) {
   alerts.push("방 온도가 지정 범위를 벗어났습니다.");
 }
 
 class HeatingPlan {
-  withinRange(bottom, top) {
+  withinRange(aNumberRange) {
     return (
-      bottom >= this._temperatureRange.low && top <= this._temperatureRange.high
+      aNumberRange.low >= this._temperatureRange.low &&
+      aNumberRange.high <= this._temperatureRange.high
     );
   }
 }
