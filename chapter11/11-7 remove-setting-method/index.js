20a7d2f5cf84c6e859ead644b306a8cb8438aa02
Eunsong-Park <run.eunsong@gmail.com>
2023-08-19 19:54:59 +0900

    feat: 11-7, 리팩터링 예시 추가

---
+++ chapter11/11-7 remove-setting-method/index.js
@@ -0,0 +1,18 @@
+class Person {
+  get name() {
+    return this._name;
+  }
+  set name(arg) {
+    this._name = arg;
+  }
+  get id() {
+    return this._id;
+  }
+  set id(arg) {
+    this._id = arg;
+  }
+}
+
+const martin = new Person();
+martin.name = "martin";
+martin.id = "1234";
