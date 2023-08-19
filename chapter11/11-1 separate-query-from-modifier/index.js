6d848cba13b83a0a833f0d07e2923cb169f3d8f9
Eunsong-Park <run.eunsong@gmail.com>
2023-08-19 12:03:26 +0900

    feat: 11-1, 리팩터링 예시 추가

--- chapter11/11-1 separate-query-from-modifier/index.js
+++ chapter11/11-1 separate-query-from-modifier/index.js
@@ -11,3 +11,5 @@ function alertForMiscreant(people) {
   }
   return "";
 }
+
+const found = alertForMiscreant(people);
--- chapter11/11-1 separate-query-from-modifier/refactoring.js
+++ chapter11/11-1 separate-query-from-modifier/refactoring.js
@@ -1,13 +1,18 @@
 function alertForMiscreant(people) {
+  if (findMiscreant(people) !== "") setOffAlarms();
+}
+
+function findMiscreant(people) {
   for (const p of people) {
     if (p === "조커") {
-      setOffAlarms();
       return "조커";
     }
     if (p === "사루만") {
-      setOffAlarms();
       return "사루만";
     }
   }
   return "";
 }
+
+const found = findMiscreant(people);
+alertForMiscreant(people);
