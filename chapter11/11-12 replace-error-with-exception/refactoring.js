ffd8fc4fb924fdae4c4530fdba00fbd14a8f9515
Eunsong-Park <run.eunsong@gmail.com>
2023-08-20 17:28:09 +0900

    feat: 11-12, 리팩터링 예시 추가

--- chapter11/11-12 replace-error-with-exception/refactoring.js
+++ chapter11/11-12 replace-error-with-exception/refactoring.js
@@ -2,15 +2,30 @@
 function localShippingRules(country) {
   const data = countryData.shippingRules[country];
   if (data) return new ShippingRules(data);
-  else return -23;
+  else throw new OrderProcessingError(-23);
 }
 
 function calculateShippingCosts(anOrder) {
   // 관련 없는 코드
   const shippingRules = localShippingRules(anOrder.country);
-  if (shippingRules < 0) return shippingRules; // 오류 전파
   // 더 관련 없는 코드
 }
 
-const status = calculateShippingCosts(orderData);
-if (status < 0) errorList.push({ order: orderData, errorCode: status });
+try {
+  calculateShippingCosts(orderData);
+} catch (e) {
+  if (e instanceof OrderProcessingError)
+    errorList.push({ order: orderData, errorCode: e.code });
+  else throw e;
+}
+
+class OrderProcessingError extends Error {
+  constructor(errorCode) {
+    super(`주문 처리 오류 ${errorCode}`);
+    this.code = errorCode;
+  }
+
+  get name() {
+    return "OrderProcessingError";
+  }
+}
