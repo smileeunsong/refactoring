447cad626c8b15f22d0aea4cfd28a07896ee3dcb
Eunsong-Park <run.eunsong@gmail.com>
2023-08-26 15:57:45 +0900

    feat: 12-01, 리팩터링 준비

---
+++ chapter12/12-01 pull-up-method/index.js
@@ -0,0 +1,13 @@
+class Party {}
+
+class Employee extends Party {
+  get annualCost() {
+    return this.monthlyCost * 12;
+  }
+}
+
+class Department extends Party {
+  get totalAnnualCost() {
+    return this.monthlyCost * 12;
+  }
+}
