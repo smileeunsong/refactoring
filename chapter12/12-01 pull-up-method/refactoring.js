95fbd77d4cdf95c9ec8f7b995cdf01c34ca7c7ea
Eunsong-Park <run.eunsong@gmail.com>
2023-08-26 16:00:39 +0900

    feat: 12-01, 리팩터링 예시 추가

--- chapter12/12-01 pull-up-method/refactoring.js
+++ chapter12/12-01 pull-up-method/refactoring.js
@@ -1,13 +1,14 @@
-class Party {}
-
-class Employee extends Party {
+class Party {
   get annualCost() {
     return this.monthlyCost * 12;
   }
-}
 
-class Department extends Party {
-  get totalAnnualCost() {
-    return this.monthlyCost * 12;
+  // 서브클래스 책임 오류
+  get monthlyCost() {
+    throw new Error("subclass responsibility");
   }
 }
+
+class Employee extends Party {}
+
+class Department extends Party {}
