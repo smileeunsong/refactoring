6e657466f2b9e1466753c11966179b3b9d5f3473
Eunsong-Park <run.eunsong@gmail.com>
2023-08-27 15:36:59 +0900

    feat: 12-07, 리팩터링 예시 추가

---
+++ chapter12/12-07 remove-subclass/index.js
@@ -0,0 +1,27 @@
+class Person {
+  constructor(name) {
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get genderCode() {
+    return "X";
+  }
+}
+
+class Male extends Person {
+  get genderCode() {
+    return "M";
+  }
+}
+
+class Female extends Person {
+  get genderCode() {
+    return "F";
+  }
+}
+
+const numberOfMales = people.filter((p) => p instanceof Male).length;
---
+++ chapter12/12-07 remove-subclass/refactoring.js
@@ -0,0 +1,35 @@
+class Person {
+  constructor(name, genderCode) {
+    this._name = name;
+    this._genderCode = genderCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get genderCode() {
+    return this._genderCode;
+  }
+
+  get isMale() {
+    return "M" === this._genderCode;
+  }
+}
+
+const numberOfMales = people.filter((p) => p.isMale).length;
+
+function createPerson(aRecord) {
+  switch (aRecord.gender) {
+    case "M":
+      return new Person(aRecord.name, "M");
+    case "F":
+      return new Person(aRecord.name, "F");
+    default:
+      return new Person(aRecord.name, "X");
+  }
+}
+
+function loadFromInput(data) {
+  return data.map((aRecord) => createPerson(aRecord));
+}
