6147211644dcd345981e2c9fbde8787a265e1641
Eunsong-Park <run.eunsong@gmail.com>
2023-08-30 00:00:36 +0900

    feat: 12-10, 리팩터링 예시 추가

---
+++ chapter12/12-10 replace-subclass-with-delegate/refactoring.js
@@ -0,0 +1,67 @@
+class Booking {
+  constructor(show, date) {
+    this._show = show;
+    this._date = date;
+  }
+
+  get hasTalkback() {
+    return this._premiumDelegate
+      ? this._premiumDelegate.hasTalkback
+      : this._show.hasOwnProperty("talkback") && !this.isPeakDay;
+  }
+
+  get basePrice() {
+    return this._premiumDelegate
+      ? this._premiumDelegate.basePrice
+      : this._privateBasePrice;
+  }
+
+  get _privateBasePrice() {
+    let result = this._show.price;
+    if (this.isPeakDay) result += Math.round(result * 0.15);
+    return result;
+  }
+
+  get hasDinner() {
+    return this._premiumDelegate ? this._premiumDelegate.hasDinner : undefined;
+  }
+
+  _bePremium(extras) {
+    this._premiumDelegate = new PremiumBookingDelegate(this, extras);
+  }
+}
+
+function createBooking(show, date) {
+  return new Booking(show, date);
+}
+
+function createPremiumBooking(show, date, extras) {
+  const result = new Booking(show, date, extras);
+  result._bePremium(extras);
+  return result;
+}
+
+class PremiumBookingDelegate {
+  constructor(hostBooking, extras) {
+    this._host = hostBooking;
+    this._extras = extras;
+  }
+
+  get hasTalkback() {
+    return this._host._show.hasOwnProperty("talkback");
+  }
+
+  get basePrice() {
+    return Math.round(this._host._privateBasePrice + this._extras.premiumFee);
+  }
+
+  get hasDinner() {
+    return this._extras.hasOwnProperty("dinner") && !this.isPeakDay;
+  }
+}
+
+// client 1
+const aBooking = createBooking(show, date);
+
+// client 2
+const aBooking = createPremiumBooking(show, date, extras);
